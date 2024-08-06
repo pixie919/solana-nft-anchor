ca18b357b95934c46f244b4144665f69229aaa11
sophie <m.n.sophie@outlook.com>
2024-08-06 16:42:26 +0300

    New file update.

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("CoFhfbiya8gRwwmqqUAukmwHYH6XabpjAEys9Cp4qGif");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
